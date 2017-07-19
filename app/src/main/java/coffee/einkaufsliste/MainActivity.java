031a46894a00df4b0265145de429d639a4df75a9
limo <lukas.hartwig@gmx.net>
2017-07-19 11:38:17 +0200

    hab was geändert

--- app/src/main/java/coffee/einkaufsliste/MainActivity.java
+++ app/src/main/java/coffee/einkaufsliste/MainActivity.java
@@ -1,45 +1,84 @@
 package coffee.einkaufsliste;
 
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linearLayoutUpperPart;
+    LinearLayout linearLayoutEntry;
+
+    EditText numberOfItems;
+    EditText editText;
+
+    class Entry {
+
+        public String number;
+        public String item;
+
+        public Entry(String number, String item) {
+            this.number = number;
+            this.item = item;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayoutUpperPart = (LinearLayout) findViewById(R.id.itemListLayout);
     }
 
     public void onClickBtn(View v) {
 
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        final EditText numberOfItems = (EditText) findViewById(R.id.editTextItemNumber);
-        final TextView textView = (TextView) findViewById(R.id.textView2);
+        numberOfItems = (EditText) findViewById(R.id.editTextItemNumber);
+        editText = (EditText) findViewById(R.id.editText);
+        linearLayoutEntry = new LinearLayout(this);
 
-        String string = textView.getText().toString();
+        //String string = "";
+        //string = string + numberOfItems.getText() + editText.getText();
 
-        String number = "";
-        switch(numberOfItems.length()) {
-            case 0: number = "  "; break;
-            case 1: number = " " + numberOfItems.getText(); break;
-            case 2: number = ""  + numberOfItems.getText(); break;
-            default: number = "  "; break;
-        }
-        string = string + numberOfItems.getText() + "\t" + editText.getText().toString() + "\n";
 
-        //Toast.makeText(this, editText.getText(), Toast.LENGTH_LONG).show();
-        textView.setText(string);
+        TextView fred = new TextView(this);
+        //fred.setHeight(100);
+        //fred.setWidth(0);
+        //fred.setGravity(3);
+        fred.setTextSize(30);
+        fred.setText(numberOfItems.getText());
+        fred.setBackgroundColor(Color.RED);
 
-        // Lukas, läuft bei dir !!!
-        // Funktioniert das jetzt auch
+        TextView frud = new TextView(this);
+        //frud.setWidth(0);
+        //frud.setGravity(7);
+        frud.setTextSize(30);
+        frud.setText(editText.getText());
+        frud.setBackgroundColor(Color.BLUE);
+        //fred.setLayoutParams(new LinearLayout.LayoutParams
+        //        (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        //fred.setBackgroundColor(Color.RED);
+        //fred.setBackgroundResource();
+        //fred.setImageDrawable(getResources().getDrawable(R.drawable.circle_image_selected));
+        //android:background="@android:drawable/editbox_dropdown_light_frame"
+
+        //fred.setText(string);
+
+        linearLayoutUpperPart.addView(linearLayoutEntry);
+        linearLayoutEntry.addView(fred);
+        linearLayoutEntry.addView(frud);
+
     }
 }
 
