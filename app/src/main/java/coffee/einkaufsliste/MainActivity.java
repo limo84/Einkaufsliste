8887d13d7ab6be1534286455979a30fb3831758a
limo <lukas.hartwig@gmx.net>
2017-07-19 14:34:03 +0200

    wixxen

--- app/src/main/java/coffee/einkaufsliste/MainActivity.java
+++ app/src/main/java/coffee/einkaufsliste/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import android.widget.TextView;
 
 // Test Titten Möpse Dödel Fetter Schwanz
+// WIXXXXEEEEn
 
 public class MainActivity extends AppCompatActivity {
 
